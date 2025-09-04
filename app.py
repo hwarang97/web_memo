d7dd33e06ce865e98520ec769a50c981b9e5de6f
dolggul2 <dolggul2@gmail.com>
2025-09-04 16:15:48 +0900

    temp

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,3 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return '<p>Hello, world!<p>'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['user_name']
+        return f'<h1>{username}님, 환영합니다!</h1>'
+    else:
+        return render_template('login.html')
